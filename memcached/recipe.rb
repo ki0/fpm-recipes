110121abebca073babb06d45b1cf34b939070bb7
Fran Rodríguez <fran.clasico@gmail.com>
2013-03-11 16:15:24 +0100

    Scripts for testing and start memcached daemmon

--- memcached/recipe.rb
+++ memcached/recipe.rb
@@ -9,8 +9,8 @@ class Memcached < FPM::Cookery::Recipe
   sha1      '12ec84011f408846250a462ab9e8e967a2e8cbbc'
 
   section   'database'
-  depends   'libevent-dev'
-
+  build_depends   'libevent-dev'
+  depends   'libevent-core-1.4-2'
   config_files '/etc/memcached.conf'
 
   def build
@@ -21,6 +21,11 @@ class Memcached < FPM::Cookery::Recipe
   def install
     (etc/'init.d').install_p(workdir/'memcached.init.d', 'memcached')
     etc.install_p(workdir/'conf.memcached', 'memcached.conf')
+    %w( memcached memcached/scripts).map do |dir|
+      (share/dir).mkpath
+    end
+    (share'/memcached/scripts').install_p(workdir/'conf.memcached-start', 'memcached-start')
+    (share'/memcached/scripts').install_p(workdir/'conf.memcached-tool', 'memcached-tool')
     make :install, 'DESTDIR' => destdir
   end
 end
